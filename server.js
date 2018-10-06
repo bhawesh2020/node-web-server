1e5ca02f31f656e9f4aa7f1a0ccebdd0030f271f
bhawesh2020 <bhawesh2020@gmail.com>
2018-10-06 23:20:25 +0530

    Printing port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs=require('fs');
 const port=process.env.PORT || 3000;
 
 var app=express();
-
+console.log("Port:",port);
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
 
